830ff67a39ab312a6a5db73583bd08c41deb70c2
Sergio Flores <sergio@saft.industries>
2023-01-12 21:31:01 -0600

    Non-concurrent implementation

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # rust-fractals
 Rendering Fractal images with Rust for fun &amp; learning
+
+### Mandelbrot
+
+First implemented, produces a PNG with a mandelbrot set in grayscale, run like so:
+
+	cargo run mandel.png 1000x750 -1.20,0.35 -1,0.20 
+
+where,   
+
+mandel.png -> Name of the output file  
+1000x750 -> Image size in pixels  
+-1.20,0.35 -> Upper left boundary of complex number space  
+-1,0.20 -> Lower right boundary of complex number space  
+
+Result:
+
+ ![Mandelbrot in grays](mandel.png "Mandelbrot in grays") 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 extern crate num_complex;
+extern crate image;
 
 use num_complex::Complex;
-use std::str::FromStr;
+
+use std::{
+	str::FromStr,
+	fs::File,
+	io::{
+		Error, ErrorKind, Result,
+	},
+};
+
+use image::{
+	ColorType,
+	codecs::png::PngEncoder
+};
+
 
 /// Given a complex number c, determine if z remains within the 2.0 radius limit or not in max limit iterations
 fn outside_two_radius_iterations_count(c: Complex<f64>, iterations_limit: u64) -> Option<u64> {
@@ -19,7 +33,7 @@ fn outside_two_radius_iterations_count(c: Complex<f64>, iterations_limit: u64) -
 	None
 }
 
-// Splits coordinates params into number component parts, like so 100x40, or 1.0,5000
+/// Splits coordinates params into number component parts, like so 100x40, or 1.0,5000
 fn parse_coordinates<T: FromStr>(s: &str, sep: char) -> Option<(T, T)> {
 
 	match s.find(sep) {
@@ -38,7 +52,7 @@ fn parse_coordinates<T: FromStr>(s: &str, sep: char) -> Option<(T, T)> {
 
 }
 
-// Given a string representing a complex, creates a complex from it
+/// Given a string representing a complex, creates a complex from it
 fn parse_complex(s: &str) -> Option<Complex<f64>> {
 
 	match parse_coordinates::<f64>(s, ',') {
@@ -48,9 +62,74 @@ fn parse_complex(s: &str) -> Option<Complex<f64>> {
 
 }
 
+/// Converts a pixel from image space to a point in number space
+///
+/// pixel is a point in the image
+/// bounds is the width and height of the image
+/// upper_left and lower_right are the end points of the number space
+fn pixel_to_point(	pixel: (f64, f64),
+					bounds: (f64, f64), 
+					upper_left: Complex<f64>,
+					lower_right: Complex<f64>) -> Complex<f64> {
+
+	let (width, height) = ( lower_right.re - upper_left.re, upper_left.im - lower_right.im);
+
+
+	Complex {
+		re: ((pixel.0 / bounds.0) * width) + upper_left.re,
+		im: ((pixel.1 / bounds.1) * height) - upper_left.im,
+	}
+}
+
+/// Given the image space params and number space params, determines the color of a pixel in image space
+fn render(pixels: &mut [u8], bounds: (usize, usize), upper_left: Complex<f64>, lower_right: Complex<f64>) {
+
+	for y in 0..bounds.1 {
+		for x in 0..bounds.0 {
+			let point = pixel_to_point(	(x as f64, y as f64), 
+										(bounds.0 as f64, bounds.1 as f64),
+										upper_left, lower_right);
+
+			pixels[(y * bounds.0) + x] = match outside_two_radius_iterations_count(point, 255 as u64) {
+				None => 0,
+				Some(count) => 255 - count as u8,
+			};
+		}
+	}
+}
+
+/// Saves image to file
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<()> {
+	let output = File::create(filename)?;
+	let encoder = PngEncoder::new(output);
+
+	match encoder.encode(&pixels, bounds.0 as u32, bounds.1 as u32, ColorType::L8) {
+		Ok(v) => v,
+		Err(_v) => return Err(Error::new(ErrorKind::Other, "oh no!")),
+	}
+
+	Ok(())
+}
+
 fn main() {
     println!("Rust Fractals Demo - Jan 12th, 2022 by sergio@saft.industries");
 	println!("Mandelbrot set");
 
-	outside_two_radius_iterations_count(Complex {re: 0.5 , im: 0.5 }, 1000 as u64);
+	let args: Vec<String> = std::env::args().collect();
+
+	if args.len() != 5 {
+		eprintln!("Usage: rust-fractals FILENAME IMAGESIZE UPPERLEFT LOWERRIGHT");
+		eprintln!("Example: rust-fractals mandel.png 1000x750 -1.20,0.35 -1,0.20");
+		std::process::exit(1);
+	}
+
+	let bounds = parse_coordinates(&args[2], 'x').expect("error with imagesize");
+	let upper_left = parse_complex(&args[3]).expect("error with upperleft");
+	let lower_right = parse_complex(&args[4]).expect("error with lowerright");
+
+	let mut pixels = vec![0; bounds.0 * bounds.1];
+
+	render(&mut pixels, bounds, upper_left, lower_right);
+
+	write_image(&args[1], &pixels, bounds).expect("error with PNG file");
 }
